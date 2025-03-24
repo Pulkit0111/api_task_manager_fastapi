2e3935393124800e20556cedb62fa198cee64fc9
Pulkit0111 <ptyagi1551@gmail.com>
2025-03-24 20:57:02 +0530

    updated the Task model

--- app/models/task.py
+++ app/models/task.py
@@ -4,7 +4,5 @@ from datetime import datetime
 class Task(BaseModel):
     title: str
     description: str
-    assigned_to: str
-    priority: int
-    due_date: datetime
+    created_by: str
     completed: bool = False
